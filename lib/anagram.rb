c0071e8e2300264c6b1dc9f9fa92d93bef491a3e
Ryna Truong <rynatruong@outlook.com>
2020-03-10 22:43:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,12 @@
 class Anagram
+  
+  attr_accessor :name
+  
   def initialize(word)
     @word = word
+  end
+  
+  def match(words)
+    words.select {|w| w.split("").sort == name.split("").sort }
   end
 end
